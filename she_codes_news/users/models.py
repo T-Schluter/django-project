e07d807afd5a410ce0e13b1c6fee6e477b693454
T-Schluter <T-Schluter@users.noreply.github.com>
2020-09-01 18:39:19 +1000

    creating form complete

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    # this is where I would put custom fields
     pass
+    this is where I would put custom fields
+    author = models.ForeignKey(
+         get_user_model(),
+         on_delete=models.CASCADE
+    )
+    
+    pub_date = models.DateTimeField()
+    content = models.TextField()
+
 
     def __str__(self):
         return self.username
